a5d12b5f8e3049cd1d4261d67f6a77f1162a9a71
hossein <h@h.com>
2025-11-03 12:24:55 +0330

    profile

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,6 @@
 # users/urls.py
 from django.urls import path
-from users.views import register, login_view, upload_students, force_password_change
+from users.views import register, login_view, upload_students, force_password_change, profile_view
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
@@ -9,5 +9,6 @@ urlpatterns = [
     path("logout/", LogoutView.as_view(next_page='users:login'), name="logout"),
     path("upload-students/", upload_students, name="upload_students"),
     path("change-password/", force_password_change, name="force_password_change"),
+    path("profile/", profile_view, name="profile"),
 
 ]
