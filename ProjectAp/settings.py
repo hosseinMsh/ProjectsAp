9de854f6c7ee16486d8848c7a6c3f459ec3ea2e5
hossein <h@h.com>
2025-11-03 12:24:01 +0330

    Force Password Change Middleware

--- ProjectAp/settings.py
+++ ProjectAp/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "users.middleware.ForcePasswordChangeMiddleware",
 ]
 
 ROOT_URLCONF = "ProjectAp.urls"
